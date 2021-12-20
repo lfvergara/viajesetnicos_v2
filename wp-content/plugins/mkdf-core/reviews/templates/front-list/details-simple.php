a38ebac78004ab3fc0722d492132aaaddb018c77
Federico Vergara <hu.ce.ro@gmail.com>
2021-12-20 07:40:31 -0300

    Haciendo detalles

--- wp-content/plugins/mkdf-core/reviews/templates/front-list/details-simple.php
+++ wp-content/plugins/mkdf-core/reviews/templates/front-list/details-simple.php
@@ -1,28 +1,32 @@
 <div class="mkdf-reviews-list-info mkdf-reviews-simple">
+    <!--
     <div class="mkdf-reviews-number-wrapper">
         <h4 class="mkdf-reviews-summary">
             <span class="mkdf-reviews-number">
-                <?php echo esc_html($rating_number); ?>
+                <?php //echo esc_html($rating_number); ?>
             </span>
             <span class="mkdf-reviews-label">
-                <?php echo esc_html($rating_label); ?>
+                <?php //echo esc_html($rating_label); ?>
             </span>
         </h4>
         <span class="mkdf-stars-wrapper">
-            <?php foreach($post_ratings as $rating) { ?>
+            <?php //foreach($post_ratings as $rating) { ?>
                 <span class="mkdf-stars-wrapper-inner">
                     <span class="mkdf-stars-items">
                         <?php
+                        /*
                         $review_rating = mkdf_core_post_average_rating($rating);
                         for ($i = 1; $i <= $review_rating; $i++) { ?>
                             <i class="fa fa-star" aria-hidden="true"></i>
-                        <?php } ?>
+                        <?php }
+                        */ ?>
                     </span>
                     <span class="mkdf-stars-label">
-                        <?php echo esc_html($rating['label']); ?>
+                        <?php //echo esc_html($rating['label']); ?>
                     </span>
                 </span>
             <?php } ?>
         </span>
     </div>
+    -->
 </div>
