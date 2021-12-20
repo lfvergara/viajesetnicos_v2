29c699b11d3a3cc50c7601efc077114380e40ff7
Federico Vergara <hu.ce.ro@gmail.com>
2021-12-20 12:37:20 -0300

    Agregando filtros al sidebar

--- wp-content/plugins/mkdf-tours/post-types/tours/templates/tour-info-parts/categories.php
+++ wp-content/plugins/mkdf-tours/post-types/tours/templates/tour-info-parts/categories.php
@@ -1,1 +1,7 @@
+<?php if(mkdf_tours_get_tour_duration()) : ?>
+	<div class="mkdf-tours-single-info-item">
+		<?php echo mkdf_tours_get_tour_duration_html(); ?>
+	</div>
+<?php endif; ?>
+
 <?php echo mkdf_tours_get_tour_categories_html(); ?>
--- wp-content/plugins/mkdf-tours/post-types/tours/templates/tour-info-parts/years.php
+++ wp-content/plugins/mkdf-tours/post-types/tours/templates/tour-info-parts/years.php
@@ -1,9 +1,3 @@
-<?php if(mkdf_tours_get_tour_duration()) : ?>
-	<div class="mkdf-tours-single-info-item">
-		<?php echo mkdf_tours_get_tour_duration_html(); ?>
-	</div>
-<?php endif; ?>
-
 <?php if(mkdf_tours_get_tour_min_age()) : ?>
 	<div class="mkdf-tours-single-info-item">
 		<?php echo mkdf_tours_get_tour_price_html(get_the_ID()); ?>
